1b6df2097d55c31188209e355c7c87f9ed9ba99e
TudorAndrei <TudorAndrei@users.noreply.github.com>
2021-10-30 22:25:58 +0300

    blog update

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-hugo -D && \
 git add . &&\
 git commit -m "blog update" && \
+git push origin main && \
+hugo -D && \
 rsync -aAXv ~/projects/blog/public/ root@tudorandrei.xyz:/home/tudor/blog/  --delete &&\
 echo "Verify at https://blog.tudorandrei.xyz"
