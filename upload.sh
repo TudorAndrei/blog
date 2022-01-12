3a846d542c67b0858cdc276979a50db120be879e
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2022-01-12 22:12:12 +0200

    blog update

--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@ git add . &&\
 git commit -m "blog update" && \
 git push origin main && \
 hugo -D && \
-rsync -aAXv ~/projects/blog/public/ root@tudorandrei.xyz:/home/tudor/blog/  --delete &&\
+rsync -aAXv ~/projects/blog/public/ tudor@tudorandrei.xyz:/home/tudor/blog/  --delete &&\
 echo "Verify at https://blog.tudorandrei.xyz"
